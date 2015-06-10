3c2e31cf4ba0fe3e9f2ce9adcba71991016e43d5
Markus Daugs <markus@mdaugs.de>
2015-06-10 18:58:21 +0200

    fixed the first task one had to handle each row of the vectors separately and visualize the result for 3 different filter-levels (directly on positions, on Euler angles and on quaternions) all in one file for now

--- tasks/task1.m
+++ tasks/task1.m
@@ -1,5 +1,5 @@
 %% task 1
-filterwidth = 8;
+filterWidth = 8;
 
 [skel,mot]=readMocap('motions/HDM_bd.asf','HDM_bd_03-03_01_120.amc') % load model
 motionplayer('skel',{skel},'mot',{mot}); % show model
@@ -7,5 +7,86 @@ motionplayer('skel',{skel},'mot',{mot}); % show model
 w = waitforbuttonpress;
 w = waitforbuttonpress;
 
-mot=filterMocap(filterwidth, skel, mot); % apply filtering
+coefs = gausswin(filterWidth)/sum(gausswin(filterWidth));
+
+for t = 1:length(mot.jointTrajectories)
+    data = cell2mat(mot.jointTrajectories(t));
+    if ~isempty(data)        
+        realD = data(1,:);
+        filterD = filter(coefs, 1, realD);
+        data(1,:)=filterD;
+        if size(data,1) > 1
+            realD = data(2,:);
+            filterD = filter(coefs, 1, realD);
+            data(2,:)=filterD;
+        end
+        if size(data,1) > 2
+            realD = data(3,:);
+            filterD = filter(coefs, 1, realD);
+            data(3,:)=filterD;
+        end
+        mot.jointTrajectories(t) = {data};
+    end
+end
+
+motionplayer('skel',{skel},'mot',{mot}); % show filtered model
+
+w = waitforbuttonpress;
+w = waitforbuttonpress;
+
+[skel,mot]=readMocap('motions/HDM_bd.asf','HDM_bd_03-03_01_120.amc') % load model
+for t = 1:length(mot.rotationEuler)
+    data = cell2mat(mot.rotationEuler(t));
+    if ~isempty(data)        
+        realD = data(1,:);
+        filterD = filter(coefs, 1, realD);
+        data(1,:)=filterD;
+        if size(data,1) > 1
+            realD = data(2,:);
+            filterD = filter(coefs, 1, realD);
+            data(2,:)=filterD;
+        end
+        if size(data,1) > 2
+            realD = data(3,:);
+            filterD = filter(coefs, 1, realD);
+            data(3,:)=filterD;
+        end
+        mot.rotationEuler(t) = {data};
+    end
+end
+mot = convert2quat(skel, mot);
+mot.jointTrajectories = forwardKinematicsQuat(skel,mot);
+
+motionplayer('skel',{skel},'mot',{mot}); % show filtered model
+
+w = waitforbuttonpress;
+w = waitforbuttonpress;
+
+[skel,mot]=readMocap('motions/HDM_bd.asf','HDM_bd_03-03_01_120.amc') % load model
+for t = 1:length(mot.rotationQuat)
+    data = cell2mat(mot.rotationQuat(t));
+    if ~isempty(data)        
+        realD = data(1,:);
+        filterD = filter(coefs, 1, realD);
+        data(1,:)=filterD;
+        if size(data,1) > 1
+            realD = data(2,:);
+            filterD = filter(coefs, 1, realD);
+            data(2,:)=filterD;
+        end
+        if size(data,1) > 2
+            realD = data(3,:);
+            filterD = filter(coefs, 1, realD);
+            data(3,:)=filterD;
+        end
+        if size(data,1) > 3
+            realD = data(4,:);
+            filterD = filter(coefs, 1, realD);
+            data(4,:)=filterD;
+        end
+        mot.rotationQuat(t) = {data};
+    end
+end
+mot.jointTrajectories = forwardKinematicsQuat(skel,mot);
+
 motionplayer('skel',{skel},'mot',{mot}); % show filtered model
